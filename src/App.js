d8abd8e658d64b5ea8e1e130a6a08528768f5979
세현 전 <shjun1210@icloud.com>
2024-12-25 22:21:36 +0900

    first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppLayout from "./layout/AppLayout";
 import HomePage from "./pages/HomePage/HomePage";
 import { Routes, Route } from "react-router-dom";
+import DetailPage from "./pages/DetailPage/DetailPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="detail/:contentid" element={<DetailPage />}/>
         </Route>
       </Routes>
     </div>
